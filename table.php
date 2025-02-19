5f189fa136dbf7c2051e1c72ad5cabacb28255de
ibmelo <ibmelo23-0338@cca.edu.ph>
2025-02-19 19:31:09 +0800

    Updated Table with console/javascript

--- table.php
+++ table.php
@@ -7,7 +7,37 @@
 </head>
 <body>
     <h1>Student Record</h1>
-    <table id="student_record">
-        
+    <table id="student_record" border="1">
+
+    </table>
+
+        <script>
+            const studentRecord = document.getElementById("student_record");
+            console.log("Hello World");
+            
+
+            const row1 = studentRecord.insertRow();
+            const row1col1 = row1.insertCell();
+            row1col1.innerHTML = "Last Name";
+
+            const row1col2 = row1.insertCell();
+            row1col2.innerHTML = "First Name";
+
+            const row2 = studentRecord.insertRow();
+            const row2col1 = row2.insertCell();
+            const row2col2 = row2.insertCell();
+
+            const inputLastName = document.createElement("INPUT");
+            inputLastName.type = "text";
+            inputLastName.placeholder = "Enter Last Name";
+
+            const inputFirstName = document.createElement("INPUT");
+            inputFirstName.type = "text";
+            inputFirstName.placeholder = "Enter First Name";
+            row2col2.append(inputFirstName);
+            row2col1.append(inputLastName);
+
+            console.log(studentRecord);
+        </script>
 </body>
 </html>
